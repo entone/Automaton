91641df1b59f88c196b97f440fc0d537abd1f430
Christopher Coté <ccote@barackobama.com>
2012-09-06 17:12:44 +0000

    fix render bug with toggle buttons

--- automaton/start_test.py
+++ automaton/start_test.py
@@ -3,4 +3,4 @@ from node.test import Test
 from node.aquaponics import Aquaponics
 
 
-aq = Manager(nodes=[Test, Aquaponics], publisher=5554, rpc=5553)
+aq = Manager(nodes=[Test, Test, Test, Test], publisher=5554, rpc=5553)
