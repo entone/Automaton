a1a2d4ec2d23ea9aeee307a4705810d10fc28021
Christopher Coté <chris@entropealabs.com>
2013-02-27 16:40:16 -0600

    update temp conversion

--- automaton/node/arduino_node.py
+++ automaton/node/arduino_node.py
@@ -133,7 +133,7 @@ class WaterLevelEvent(SensorEvent): pass
 
 class Temperature(Sensor):
     def conversion(self, value):
-        mV = value * (5000/1024)
+        mV = value * (3300/1024)
         return (mV-500)/10
 
 class Humidity(Sensor):
