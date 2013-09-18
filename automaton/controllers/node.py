1dbdb338b05b2598eb75749116135998788b4e89
Christopher Coté <chris@entropealabs.com>
2013-09-18 19:09:59 +0000

    initial websocket support

--- automaton/controllers/node.py
+++ automaton/controllers/node.py
@@ -1,6 +1,8 @@
-from flask import current_app, request, Blueprint
+from flask import current_app, request, Blueprint, render_template, Response
+import gevent
 from automaton import settings
-import sqlite3
+import random
+import json
 
 node = Blueprint(
     'node',
@@ -10,51 +12,19 @@ node = Blueprint(
 
 @node.route('/', methods=['GET'])
 def display():
-    res = {}
-    self.rpc = RPC(port=settings.CLIENT_RPC, address='127.0.0.1')
-    res = self.rpc.send(dict(method='get_nodes'), settings.KEY)
-    db = sqlite3.connect("automaton.db", detect_types=sqlite3.PARSE_DECLTYPES)
-    cur = db.cursor()
-    q_t = datetime.datetime.utcnow()-datetime.timedelta(hours=settings.HISTORICAL_DISPLAY)
-    for n in res:
-        n['historical'] = cur.execute('SELECT * FROM logs WHERE node=? AND timestamp > ? LIMIT 20', (n.get('name'),q_t)).fetchall()
-    
-    current_app.logger.debug("Got Nodes: %s" % res)
-    db.close()
-    return render_template("base.html", values=json.dumps(res, cls=ComplexEncoder))
+    return render_template("stream.html");
 
-@node.route('/historical/<name>', methods=['GET'])
-def historical(name):
-    db = sqlite3.connect("automaton.db", detect_types=sqlite3.PARSE_DECLTYPES)
-    cur = db.cursor()
-    q_t = datetime.datetime.utcnow()-datetime.timedelta(hours=settings.HISTORICAL_DISPLAY)
-    res = cur.execute('SELECT * FROM logs WHERE node=? AND timestamp > ?', (name,q_t)).fetchall()
-    self.logger.debug(res)
-    return Response(json.dumps(res, cls=ComplexEncoder))
-
-@node.route('/index', methods=['GET'])
-def index():
-    ws = request.environ['wsgi.websocket']
-    def write_out(ob):
+@node.route('/stream/')
+def stream():
+    try:
+        ws = request.environ['wsgi.websocket']
+        current_app.logger.info(ws)
+    except Exception as e:
+        current_app.logger.exception(e)    
+    while True:
         try:
-            st = "%s\n" % json.dumps(ob, cls=ComplexEncoder)
-            self.logger.debug("Message: %s" % st)
-            ws.send(st)
-            return True
+            ws.send(json.dumps({"int":random.randint(1, 10000)}))
         except Exception as e:
-            self.logger.info("Connection Closed: %s" % ws)
-            return False
-    sub = Subscriber(callback=write_out, port=settings.CLIENT_SUB, broadcast=False, spawn=False, parse_message=parse_message)
-    return Response('')
-    
-def parse_message(message):
-    return aes.decrypt(message, settings.KEY)
-
-@node.route('/control', methods=['GET'])
-def control():
-    rpc = RPC(port=settings.CLIENT_RPC)
-    mes = request.json()
-    mes['method'] = 'set_output_state'
-    res = rpc.send(mes, settings.KEY)
-    return Response(json.dumps(res, cls=ComplexEncoder))
+            return Response()
+        gevent.sleep(1)
 
--- automaton/settings/__init__.py
+++ automaton/settings/__init__.py
@@ -7,6 +7,4 @@ try:
     if env == 'test': from test import *
     if env == 'staging': from staging import *
     if env == 'production': from production import *
-    print "Imported settings for %s environment" % env
-except Exception as e:
-    print "ENVY_ENV not set, using base settings."
+except Exception as e:pass
